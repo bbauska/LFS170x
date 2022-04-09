884cb7380becbae1573db17bf8823e54b884d6cf
Brian Bauska <brianbauska@gmail.com>
2022-04-09 09:29:09 -0700

    Create function.js

---
+++ js/function.js
@@ -0,0 +1,7 @@
+/** Called from index.html within bbauska/CandysRoof.bauska.site/main.  Playing and learning JS.  */
+
+function myfunction()   // write shit.
+ {   
+document.write("welcome to Learning from Brian.  Pick it up as I go along.  Love the shit out of git.");  
+         } 
+<a href = "javascript:history.back()">Back to previous page</a>
