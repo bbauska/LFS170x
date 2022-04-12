ccc7d166573fa70372f09ab44efe76765a0ffcaf
Brian Bauska <brianbauska@gmail.com>
2022-04-12 12:15:40 -0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,4 @@
-/** Called from index.html within bbauska/lfs170x.bauska.org/main.  Playing and learning JS.  */
-<!--------------------------------------------------------->
-<!------------ script function: scrollFunction() ---------->
-
-//Get the button
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-/* was scrollTop > 10 & 20 */
-function scrollFunction() {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-</script>
+/** Called from index.html within bbauska/CandysRoof.bauska.site/main.  Playing and learning JS.  */
 
 function myfunction()   // write shit.
  {   
